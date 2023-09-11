f9df5bdc2fcb33c050495f3c0cbcb82f28742423
César Claros <cesarclarosns@gmail.com>
2023-09-10 18:00:35 -0600

    Add update service

---
+++ src/app/services/prompt-update.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PromptUpdateService {
+  constructor(swUpdate: SwUpdate) {
+    swUpdate.versionUpdates
+      .pipe(
+        filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+      )
+      .subscribe((evt) => {
+        document.location.reload();
+      });
+  }
+}
